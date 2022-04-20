99177260c19a8edf66756ddd92da6550b0fa8029
Hunt092 <hunters.hunt92@gmail.com>
2022-04-20 13:38:59 +0530

    basic Markup and Backend Connectivity done

--- app.py
+++ app.py
@@ -3,22 +3,18 @@ import pickle
 import numpy as np
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl','rb'))
+model = pickle.load(open("model.pkl", "rb"))
 
-@app.route('/')
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
-
-
-@app.route('/api',methods=['POST'])
-def predict():
+    if request.method == "GET":
+        return render_template("index.html")
     # Get the data from the POST request.
-    data = request.get_json(force=True)
+    data = request.form
     # Make prediction using model loaded from disk as per the data.
-    prediction = model.predict([[np.array(data['exp'])]])
-    # Take the first value of prediction
-    output = prediction[0]
-    return jsonify(output)
+    print(data)
+
 
 if __name__ == "__main__":
-  app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True)
