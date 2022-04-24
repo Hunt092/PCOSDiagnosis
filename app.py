62b69bb2c895bd5dc59db6fc4fe3c266a7bc6125
Hunt092 <hunters.hunt92@gmail.com>
2022-04-24 11:09:49 +0530

    Basic Project done

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
+from black import out
 from flask import Flask, jsonify, render_template, request
 import pickle
 import numpy as np
 
 app = Flask(__name__)
-model = pickle.load(open("model.pkl", "rb"))
+model = pickle.load(open("bigboymodel.pkl", "rb"))
+
+
+def seletect(x):
+    try:
+        y = int(x)
+        return y
+    except:
+        try:
+            y = float(x)
+            return y
+        except:
+            return x
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -12,10 +25,15 @@ def index():
         return render_template("index.html")
     # Get the data from the POST request.
     data = [x for x in request.form.values()]
-
+    numarray = np.array(data).reshape(1, -1)
     # Make prediction using model loaded from disk as per the data.
-    print(data)
-    return render_template("index.html")
+    answer = model.predict(numarray)
+
+    if answer[0]:
+        output = True
+    else:
+        output = False
+    return render_template("index.html", output=output)
 
 
 if __name__ == "__main__":
